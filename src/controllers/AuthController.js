c061de511a6cf679fe2b1014b351d58faeaf9f2e
Crisley Alves <crisleyalvesphx@gmail.com>
2020-03-15 16:02:09 -0300

    Add request validation for authentication

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -1,8 +1,21 @@
+const { check, validationResult } = require('express-validator/check');
 const AuthRepository = require("../repository/AuthRepository");
 
-module.exports = {
-  async login(req, res) {
+// @TODO
+// Maybe move this validation module
+
+const login = {
+  validations: [
+    check('email').isEmail().withMessage('invalid email sent'),
+    check('password').isString(),
+  ],
+  handler: async(req, res) => {
     const { password, email } = req.body;
+
+    const schemaErrors = validationResult(req);
+		if (!schemaErrors.isEmpty()) {
+			return res.status(403).send(schemaErrors.array());
+		}
 
     const token = await AuthRepository.authenticate(email, password);
     if(!token) return res.status(401).json({ data: { message: 'Invalid credentials' } });
@@ -10,3 +23,7 @@ module.exports = {
     return res.status(200).json({ data: { token } });
   }
 }
+
+module.exports = {
+  login,
+};
--- src/routes.js
+++ src/routes.js
@@ -18,7 +18,7 @@ routes.get('/api/users/:user_id/techs', TechController.index);
 routes.post('/api/users/:user_id/techs', TechController.save);
 routes.delete('/api/users/:user_id/techs', TechController.delete);
 
-routes.post('/api/auth/login', AuthController.login);
+routes.post('/api/auth/login', AuthController.login.validations, AuthController.login.handler);
 
 routes.get('/api/companies', CompanyController.index);
 routes.post('/api/companies', CompanyController.save);
