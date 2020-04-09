6d570ec6b53a36d42a1d548cb6ec1faee14dfcbc
Crisley Alves <crisleyalvesphx@gmail.com>
2020-04-08 21:10:27 -0300

    Add tests for when user tries to delete an address

--- src/modules/address/__tests__/address.test.js
+++ src/modules/address/__tests__/address.test.js
@@ -2,7 +2,16 @@ const request = require('supertest');
 const { expect } = require('chai');
 const app = require('../../../app');
 
+const { removeAddressSuccess, removeAddressError, serverError } = require('../../shared/messages');
+
 const FIRST_USER_ID = 1;
+const FIRST_ADDRESS_ID = 1;
+
+const INVALID_USER_ID = 99;
+const INVALID_ADDRESS_ID = 99;
+
+const NULL_USER_ID = null;
+const NULL_ADDRESS_ID = null;
 
 describe('Address module', () => {
   it('should add an address to a user', async () => {
@@ -22,5 +31,32 @@ describe('Address module', () => {
     expect(response.body.data.zipcode).to.equal(address.zipcode);
     expect(response.body.data.street).to.equal(address.street);
     expect(response.body.data.number).to.equal(address.number);
+  });
+
+  it('should remove the given user\'s address', async () => {
+    const response = await request(app)
+      .delete(`/api/users/${FIRST_USER_ID}/addresses/${FIRST_ADDRESS_ID}`)
+      .expect('Content-Type', /json/)
+      .expect(200);
+
+    expect(response.body.data.message).to.equal(removeAddressSuccess);
+  });
+
+  it('should return a message that the given address does not exist', async () => {
+    const response = await request(app)
+      .delete(`/api/users/${INVALID_USER_ID}/addresses/${INVALID_ADDRESS_ID}`)
+      .expect('Content-Type', /json/)
+      .expect(404);
+
+    expect(response.body.data.message).to.equal(removeAddressError);
+  });
+
+  it('should return server error mesage', async () => {
+    const response = await request(app)
+      .delete(`/api/users/${NULL_USER_ID}/addresses/${NULL_ADDRESS_ID}`)
+      .expect('Content-Type', /json/)
+      .expect(500);
+
+    expect(response.body.message).to.equal(serverError);
   });
 });
