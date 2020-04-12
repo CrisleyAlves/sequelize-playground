40808caddd6152299c4b95953bc7d165aeb57219
Crisley Alves <crisleyalvesphx@gmail.com>
2020-04-11 21:31:23 -0300

    Add tests for company module

--- src/tests/index.js
+++ src/tests/index.js
@@ -2,3 +2,4 @@ require('./modules/user/user.test');
 require('./modules/authentication/authentication.test');
 require('./modules/address/address.test');
 require('./modules/tech/tech.test');
+require('./modules/company/company.test');
---
+++ src/tests/modules/company/data/index.js
@@ -0,0 +1,12 @@
+const company = {
+  name: "Company name",
+  logo_url: "logo url",
+  business_name: "Business Name",
+  description: "Company description",
+  email: "email@company.com",
+  password: "password"
+};
+
+module.exports = {
+  company,
+};
