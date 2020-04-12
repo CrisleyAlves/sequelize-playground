6f4a1fea2b0072993cbc21d9d42f0a24efbfc0fd
Crisley Alves <crisleyalvesphx@gmail.com>
2020-04-12 13:07:08 -0300

    Add setup Sentry configuration

--- src/app.js
+++ src/app.js
@@ -1,11 +1,25 @@
 const express = require("express");
 const routes = require("./routes");
+const Sentry = require('@sentry/node');
+
+// @TODO
+// Create .env file and get project configuration from there
+const { dsn } = require('./sentry');
+
+if (process.env.NODE_ENV !== 'test') {
+  Sentry.init({
+    environment: 'development',
+    dsn,
+  });
+}
 
 require('./database');
 
 const app = express();
 
+app.use(Sentry.Handlers.requestHandler());
 app.use(express.json());
 app.use(routes);
+app.use(Sentry.Handlers.errorHandler());
 
 module.exports = app;
