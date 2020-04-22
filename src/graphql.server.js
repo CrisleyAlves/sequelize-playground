a234fd55bf9234ecf9e18f8baa43f230a48833f1
Crisley Alves <crisleyalvesphx@gmail.com>
2020-04-22 18:55:58 -0300

    Add sentry import in graphql server

--- src/graphql.server.js
+++ src/graphql.server.js
@@ -1,5 +1,7 @@
 const { ApolloServer } = require('apollo-server-express');
+
 const { resolvers, typeDefs } = require('./graphql')
+const sentry = require('./sentry');
 
 const server = new ApolloServer({
   typeDefs,
