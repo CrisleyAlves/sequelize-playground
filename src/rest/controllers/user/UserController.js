5c1d89f662da8cae479424f4cd919fb3880debf3
Crisley Alves <crisleyalvesphx@gmail.com>
2020-04-23 21:25:34 -0300

    Add apply http util in user controller in rest

--- src/rest/controllers/user/UserController.js
+++ src/rest/controllers/user/UserController.js
@@ -1,6 +1,8 @@
-const UserRepository = require("@repositories/UserRepository");
 const { generatePasswordHash } = require("@utils/bcrypt");
 const { emailAlreadyInUse } = require("@utils/messages");
+const { ok, conflict, serverError } = require("@utils/http");
+
+const UserRepository = require("@repositories/UserRepository");
 
 const { validations } = require('./UserControllerValidation');
 
@@ -8,8 +10,12 @@ const user = {
   validations: validations.save,
   handler: {
     async index(req, res) {
-      const users = await UserRepository.getAll();
-      return res.status(200).json({ data: users });
+      try {
+        const users = await UserRepository.getAll();
+        return res.status(200).json(ok(users));
+      } catch (error) {
+        return res.status(500).json(serverError());
+      }
     },
     async save(req, res) {
       const { name, email, password } = req.body;
@@ -18,7 +24,7 @@ const user = {
         const user = await UserRepository.userExists(email);
 
         if (user) {
-          return res.status(409).json({ data: { message: emailAlreadyInUse } });
+          return res.status(409).json(conflict(emailAlreadyInUse));
         }
 
         const passwordHash = await generatePasswordHash(password);
@@ -28,9 +34,9 @@ const user = {
           password: passwordHash,
         });
 
-        return res.status(200).json({ data: { user: createdUser } });
+        return res.status(200).json(ok(createdUser));
       } catch (error) {
-        return res.status(500).json({ error });
+        return res.status(500).json(serverError());
       }
     },
   }
